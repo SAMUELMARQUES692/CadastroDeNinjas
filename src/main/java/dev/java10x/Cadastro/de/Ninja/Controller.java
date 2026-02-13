0d04b62b89679d0989b9f82198fc8e3207821cc4
samuel <samue@Samuel>
2026-02-12 22:22:11 -0300

    Criando a classe Controller

---
+++ src/main/java/dev/java10x/Cadastro/de/Ninja/Controller.java
@@ -0,0 +1,16 @@
+package dev.java10x.Cadastro.de.Ninja;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Essa é minha primeira mensagem nesta rota";
+    }
+
+}
