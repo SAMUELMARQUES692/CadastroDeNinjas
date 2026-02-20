41ff73a7201cb17a3f98a466e527a0d31ca6b995
samuel <samue@Samuel>
2026-02-19 22:09:29 -0300

    feat: Adiciona novas rotas a classe MissoesController e cria a logica de negocio na classe MissoesModel

--- src/main/java/dev/java10x/Cadastro/de/Ninja/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/Cadastro/de/Ninja/Missoes/MissoesService.java
@@ -1,4 +1,34 @@
 package dev.java10x.Cadastro.de.Ninja.Missoes;
 
+import dev.java10x.Cadastro.de.Ninja.Ninjas.NinjaModel;
+
+import java.util.List;
+
 public class MissoesService {
+
+    private MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    public MissoesModel criarMissao(MissoesModel missoes) {
+        return missoesRepository.save(missoes);
+    }
+
+    public List<MissoesModel> listarMissoes() {
+        return missoesRepository.findAll();
+    }
+
+    public MissoesModel atualizarMissao(Long id, MissoesModel missoesAtualizada) {
+        if (missoesRepository.existsById(id)) {
+            missoesAtualizada.setId(id);
+            return missoesRepository.save(missoesAtualizada);
+        }
+        return null;
+    }
+
+    public void deletarMissao(Long id) {
+         missoesRepository.deleteById(id);
+    }
 }
